d0d8ae7ac7b9cfcf096f610bd8282fefadecd275
itmonksakshay <akshay.bhatt@hotmail.com>
2024-08-06 16:44:15 +0530

    tailwind css layout setup

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div>Header</div>
+      <div>{children}</div>
+      <div>Footer</div>
+    </div>
+  );
+};
+
+export default Layout;
