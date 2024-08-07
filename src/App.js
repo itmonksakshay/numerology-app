7da6def91e54d98229cff2de4c993011a2be5c2b
itmonksakshay <akshay.bhatt@hotmail.com>
2024-08-07 13:28:03 +0530

    Info Card Implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import "./App.css";
 import Layout from "./components/layout";
 import InfoCard from "./components/molecules/InfoCard";
+import infoCardPattern from "./assets/images/infoCardPattern.png";
+import { styled } from "@mui/material";
+console.log(infoCardPattern, "infoCardPattern");
+
+const TextWrapper = styled("div")({
+  "& .title": {
+    fontSize: "16px",
+    fontWeight: 600,
+    lineHeight: "19.36px",
+    color: "white",
+  },
+  "& .subtitle": {
+    fontSize: "18px",
+    fontWeight: 600,
+    lineHeight: "21.78px",
+    color: "#FB6514",
+  },
+});
+
 function App() {
+  const infoData = [
+    { title: "Title 1", subtitle: "title 1 description" },
+    { title: "Date", subtitle: "07/08/2024" },
+    { title: "Title 3", subtitle: "title 3" },
+  ];
+
   return (
     <Layout>
-      <InfoCard title={"Title 1"}>
-        <h2>level</h2>
-      </InfoCard>
+      <div className="flex gap-[24px]">
+          <InfoCard title={"Title 1"} className="min-w-[248px]">
+            <div className="px-[25px] py-3">
+              <div className="w-[198px] h-[195px] flex flex-col items-center justify-between">
+                {infoData.map((item) => (
+                  <TextWrapper className="flex flex-col gap-[8px] items-center">
+                    <p className="title">{item.title}</p>
+                    <p className="subtitle">{item.subtitle}</p>
+                  </TextWrapper>
+                ))}
+              </div>
+            </div>
+          </InfoCard>
+          <InfoCard title={"Title 2"} className="min-w-[248px]">
+            <div
+              className="w-[238px] h-[240px]"
+              style={{ backgroundImage: `url(${infoCardPattern})` }}
+            >
+              <p className="text-[150px] flex justify-center items-center font-semibold text-white leading-[181.53px]">
+                5
+              </p>
+            </div>
+          </InfoCard>
+        <InfoCard title={"Title 3"} className="min-w-[656px] flex-auto"></InfoCard>
+      </div>
     </Layout>
   );
 }
--- src/components/molecules/InfoCard.jsx
+++ src/components/molecules/InfoCard.jsx
@@ -23,17 +23,31 @@ const CardWrapper = styled("div")({
       fontWeight: 600,
       lineHeight: "24.2px",
     },
+    "& .start-bar":{
+        width:'100%',
+        border: "0.65px solid",
+        borderImageSlice:1,
+        borderImageSource:"linear-gradient(90deg, rgba(229, 201, 169, 0) 0%, #E5C9A9 100%)"
+    },
+    "& .end-bar":{
+        width:'100%',
+        border: "0.65px solid",
+        borderImageSlice:1,
+        borderImageSource:"linear-gradient(90deg, #E5C9A9 0%, rgba(229, 201, 169, 0) 100%)"
+    }
   });
 
-const InfoCard =({children,title})=>{
-    return       <CardWrapper className="w-[248px] h-[401px] rounded-xl">
-    <div className="h-[90px] flex items-center justify-center card-title text-white	">
+const InfoCard =({children,title,...extraProps})=>{
+    return       <CardWrapper className="rounded-xl"{ ...extraProps}>
+    <div className="h-[72px] flex items-center justify-center card-title text-white	">
       {title}
     </div>
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center gap-4 mx-4">
+        <div className="start-bar"></div>
       <img src={threeStarIcon} />
+      <div className="end-bar"></div>
     </div>
-    <div className="pt-[40px]">
+    <div className="pt-8">
       {children}
     </div>
   </CardWrapper>
