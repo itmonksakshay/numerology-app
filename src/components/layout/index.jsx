85e219bf4cc1bf299d4f80fdf2431b8ecd38ad4d
Sahil bhatt <sahilbhatt540@gmail.com>
2024-08-07 13:29:17 +0530

    footer-component

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import backgroundImage1 from "../../assets/images/backgrounImage1.png";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="w-full">
-       <Header/>
+      <Header />
       <div className="relative w-full">
         <img src={backgroundImage1} className="absolute w-full z-0" />
         <div className="px-[120px] py-[84px] absolute">{children}</div>
       </div>
-      <div>Footer</div>
+      <Footer/>
     </div>
   );
 };
