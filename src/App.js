d2e80ee00233a68398ff91b726b355911c945247
itmonksakshay <akshay.bhatt@hotmail.com>
2025-07-12 13:01:42 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import NumerologyDetail from "./pages/numerologyDetail/NumerologyDetail";
 import About from "./pages/about/About";
 import NoMatch from "./pages/noMatch/NoMatch";
 
-
 function App() {
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Layout />}>
+      <Routes>
+        <Route path='/' element={<NumerologyDetail />}>
           <Route index element={<NumerologyDetail />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NoMatch />} />
+          <Route path='about' element={<About />} />
+          <Route path='*' element={<NoMatch />} />
         </Route>
       </Routes>
     </>
