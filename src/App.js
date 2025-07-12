654371f66fec9afe9b142b99dc820d7a8e89d76d
itmonksakshay <akshay.bhatt@hotmail.com>
2025-07-12 13:49:13 +0530

    app fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<NumerologyDetail />}>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<NumerologyDetail />} />
           <Route path='about' element={<About />} />
           <Route path='*' element={<NoMatch />} />
         </Route>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/numerology-app'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
