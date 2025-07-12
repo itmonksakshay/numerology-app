7101d8041dedfed084323cd954da89628cba4124
itmonksakshay <akshay.bhatt@hotmail.com>
2025-07-12 13:40:03 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<NumerologyDetail />}>
-          <Route index element={<NumerologyDetail />} />
           <Route path='about' element={<About />} />
           <Route path='*' element={<NoMatch />} />
         </Route>
